627190c4d5cb80e04a6105c59f74505604357191
YippyYaYa <yippyyaya1996@gmail.com>
2024-02-10 01:30:05 +0800

    [feat] finished services page [feat] WIP added gallery page [feat] WIP added home page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/shared/shared.module';
 import { HomeRouteModule } from './home.route';
+import { AboutModule } from '../about/about.module';
+import { ServicesModule } from '../services/services.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { HomeRouteModule } from './home.route';
   ],
   imports: [
     SharedModule,
-    HomeRouteModule
+    HomeRouteModule,
+    AboutModule,
+    ServicesModule,
   ],
   providers: [],
   exports: [
